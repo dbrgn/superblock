4b0bef90d9aa2d951549a30e32630beef33630ae
Danilo Bargen <gezuru@gmail.com>
2012-12-22 14:55:23 +0100

    Improved output formatting

--- superblock.py
+++ superblock.py
@@ -8,33 +8,20 @@ Usage:
 import sys
 import string
 from binascii import hexlify
-from datetime import datetime, timedelta
-
+from datetime import datetime
 
 BLOCKSIZE = 512
 
 
-def nonprintable_replace(char):
-    if char not in string.printable:
-        return '.'
-    if char in '\n\r\t\x0b\x0c':
-        return '.'
-    return char
-
-
-def lsb2int(b_string):
-    """Take a binary string (from ``file.read()``) and convert it into a
-    integer by assuming 2 byte little endian byte order."""
-    msb_string = hexlify(b_string)
-    lsb_string = ''.join([msb_string[x:x+2] for x in range(0, len(msb_string), 2)][::-1])
-    return int(lsb_string, 16)
-
-
-def timestamp(seconds):
-    return datetime.fromtimestamp(seconds)
-    
-
 def dump(filename):
+
+    def nonprintable_replace(char):
+        if char not in string.printable:
+            return '.'
+        if char in '\n\r\t\x0b\x0c':
+            return '.'
+        return char
+
     with open(filename, 'rb') as f:
         f.seek(2 * BLOCKSIZE)
         for i in xrange(BLOCKSIZE / 16):
@@ -45,25 +32,64 @@ def dump(filename):
 
 
 def analyze(filename):
+
+    # Binary conversion functions
+
+    def lsb2hex(b_string):
+        """Take a binary string (from ``file.read()``) and convert it into a
+        hex string by assuming 2 byte little endian byte order."""
+        msb_string = hexlify(b_string)
+        lsb_string = ''.join([msb_string[x:x + 2] for x in range(0, len(msb_string), 2)][::-1])
+        return lsb_string
+
+    def lsb2ascii(b_string):
+        """Take a msb hex string and convert it to an ascii string."""
+        msb_string = hexlify(b_string)
+        pairs = (msb_string[x:x + 2] for x in range(0, len(msb_string), 2))
+        values = (int(x, 16) for x in pairs)
+        return ''.join(map(chr, values))
+
+    def lsb2int(b_string):
+        """Take a binary string (from ``file.read()``) and convert it into a
+        integer by assuming 2 byte little endian byte order."""
+        lsb_string = lsb2hex(b_string)
+        return int(lsb_string, 16)
+
+    # Formatting functions
+
+    def uuid(h_string):
+        """Format a hex string like an UUID."""
+        split = lambda x: [x[:8], x[8:12], x[12:16], x[16:20], x[20:]]
+        return '-'.join(split(h_string))
+
+    def timestamp(seconds):
+        return datetime.fromtimestamp(seconds)
+
     with open(filename, 'rb') as f:
         f.seek(2 * BLOCKSIZE)
-        
+
+        # Bytes 0-15
         print 'Total number of inodes: {0:d}'.format(lsb2int(f.read(4)))
         print 'Filesystem size in blocks: {0:d}'.format(lsb2int(f.read(4)))
         print 'Number of reserved blocks: {0:d}'.format(lsb2int(f.read(4)))
         print 'Free blocks counter: {0:d}'.format(lsb2int(f.read(4)))
 
+        # Bytes 16-31
         print 'Free inodes counter: {0:d}'.format(lsb2int(f.read(4)))
         print 'Number of first block: {0:d}'.format(lsb2int(f.read(4)))
-        print 'Block size: {0:d}'.format(lsb2int(f.read(4)))
+        val = lsb2int(f.read(4))
+        print 'Block size: {0:d} ({1:d} Byte)'.format(val, 1024 * 2 ** val)
         print 'Fragment size: {0:d}'.format(lsb2int(f.read(4)))
 
+        # Bytes 32-47
         print 'Number blocks per group: {0:d}'.format(lsb2int(f.read(4)))
         print 'Number fragments per group: {0:d}'.format(lsb2int(f.read(4)))
         print 'Number inodes per group: {0:d}'.format(lsb2int(f.read(4)))
-        print 'Time of last mount: {:%Y-%m-%d %H:%M:%S}'.format(timestamp(lsb2int(f.read(4))))
+        val = lsb2int(f.read(4))
+        print 'Time of last mount: {0:d} ({1:%Y-%m-%d %H:%M:%S})'.format(val, timestamp(val))
 
-        print 'Time of last write: {:%Y-%m-%d %H:%M:%S}'.format(timestamp(lsb2int(f.read(4))))
+        # Bytes 48-63
+        print 'Time of last write: {0:%Y-%m-%d %H:%M:%S}'.format(timestamp(lsb2int(f.read(4))))
         print 'Mount operations counter: {0:d}'.format(lsb2int(f.read(2)))
         print 'Number of mount operations before check: {0:d}'.format(lsb2int(f.read(2)))
         print 'Magic signature: {0:#X}'.format(lsb2int(f.read(2)))
@@ -71,11 +97,13 @@ def analyze(filename):
         print 'Behavior when detecting errors: {0:d}'.format(lsb2int(f.read(2)))
         print 'Minor revision level: {0:d}'.format(lsb2int(f.read(2)))
 
-        print 'Time of last check: {:%Y-%m-%d %H:%M:%S}'.format(timestamp(lsb2int(f.read(4))))
+        # Bytes 64-79
+        print 'Time of last check: {0:%Y-%m-%d %H:%M:%S}'.format(timestamp(lsb2int(f.read(4))))
         print 'Time between checks: {0:d}'.format(lsb2int(f.read(4)))
         print 'OS Filesystem created: {0:d}'.format(lsb2int(f.read(4)))
         print 'Revision level: {0:d}'.format(lsb2int(f.read(4)))
 
+        # Bytes 80-95
         print 'Default user ID for reserved blocks: {0:d}'.format(lsb2int(f.read(2)))
         print 'Default group ID for reserved blocks: {0:d}'.format(lsb2int(f.read(2)))
         print 'Number first nonreserved inode: {0:d}'.format(lsb2int(f.read(4)))
@@ -83,13 +111,20 @@ def analyze(filename):
         print 'Block group number of this superblock: {0:d}'.format(lsb2int(f.read(2)))
         print 'Compatibility features bitmap: {0:#X}'.format(lsb2int(f.read(4)))
 
+        # Bytes 96-103
         print 'Incompatible features bitmap: {0:#X}'.format(lsb2int(f.read(4)))
         print 'Read-only compatible features bitmap: {0:#X}'.format(lsb2int(f.read(4)))
 
-        print '128-bit filesystem identifier: {0:#X}'.format(lsb2int(f.read(16)))
-        print 'Volume name: {0:#X}'.format(lsb2int(f.read(16)))
-        print 'Path of last mount point: {0:#X}'.format(lsb2int(f.read(64)))
+        # Bytes 104-119
+        print '128-bit filesystem identifier: {0}'.format(uuid(hexlify(f.read(16))))
 
+        # Bytes 120-135
+        print 'Volume name: {0}'.format(lsb2ascii(f.read(16)))
+
+        # Bytes 136-199
+        print 'Path of last mount point: {0}'.format(lsb2ascii(f.read(64)))
+
+        # Bytes 200-207
         print 'Compression: {0:#X}'.format(lsb2int(f.read(8)))
         print 'Number of blocks to preallocate: {0:d}'.format(lsb2int(f.read(1)))
         print 'Number of blocks to preallocate for directories: {0:d}'.format(lsb2int(f.read(1)))
